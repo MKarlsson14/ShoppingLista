5d83c249d9a709cc8e58f02fe7aaa58146f14fb1
mKarlsson14 <mackankb98@hotmail.se>
2024-12-18 15:08:39 +0100

    Just some code cleanup

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,16 +5,13 @@ using ShoppingLista.ViewModels;
 namespace ShoppingLista
 {
     public partial class MainPage : ContentPage
-    {
-        private MainViewModel vmPrivate;      
-
+    {    
         public MainPage(MainViewModel vm)
         {
             InitializeComponent();           
             BindingContext = vm;
             vm.Title = "Mackes Shopping";
-            vmPrivate = vm;           
+                   
         }
     }
-
 }
